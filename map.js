de2772331082e64a1b2a55da1eb7900b9953a8b5
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-16 18:23:22 -0600

    getting the firdt index of words

---
+++ map.js
@@ -0,0 +1,22 @@
+const map = function(array, callback) {
+
+    const results = [];
+    for (let item of array) {
+      results.push(callback(item));
+    }
+    return results;
+  
+  };
+  
+
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
