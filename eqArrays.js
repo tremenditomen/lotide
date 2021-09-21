287fe93f71e8de2829eacdd1ba07fd1dc7c56140
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-21 15:30:37 -0600

    refractoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,15 @@
-
 const eqArrays = function (arr1, arr2) {
     for (i = 0; i < arr2.length; i++) {
       if (arr1[i]!==arr2[i]){
-  
-          return false
+          
+        console.log(`${arr1}!==${arr2}`)
+        
+          return false 
+
       }
     }
-    return true 
+    console.log(`${arr1}===${arr2}`)
+    return true
   };
-  
-  console.log(eqArrays([1, 2, 3], [1, 2, 3])) // true
-  console.log(eqArrays([1, 2, 3], [3, 2, 1])) // false
-  console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))// true
-  console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))// false
-  
+  module.exports = eqArrays;
+ 
