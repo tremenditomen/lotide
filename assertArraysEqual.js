40cdd4546e372971eb48aef1c6a581b38f65bf11
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-21 15:33:35 -0600

    rrefractoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,9 @@
-const eqArrays = function (arr1, arr2) {
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1[i]!==arr2[i]){
-
-        return false
-    }
-  }
-  return true 
-};
-
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1 , arr2)) {
-     console.log(`✅✅✅ assertion passed ${arr1} === ${arr2}`);
-  
-    } else {
-      console.log(`🛑🛑🛑 assertion failed ${arr1} !=== ${arr2}`);
-    }
+  if (eqArrays(arr1, arr2)) {
+    return console.log(`✅✅✅ assertion passed ${arr1} === ${arr2}`);
+  } else {
+    return console.log(`🛑🛑🛑 assertion failed ${arr1} !== ${arr2}`);
+  }
 };
- assertArraysEqual([1,2,3],[1,2,3])
+module.exports = assertArraysEqual;
