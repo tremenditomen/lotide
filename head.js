20b68f0bc1f43335b316703a30526cfd763a7bf5
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-21 15:31:00 -0600

    refractoring

--- head.js
+++ head.js
@@ -1,22 +1,11 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
-  /*if (actual === expected) {
-      return console.log(`✅✅✅ assertion passed ${actual} === ${expected}`);
-  
-    } else {
-      console.log(`🛑🛑🛑 assertion failed ${actual} !==== ${expected}`);
-    }*/
-};
+
+const assertEqual = require ('./assertEqual')
+
 const head = (array)=>{
   console.log(array [0]);
 };
  
+  module.exports = head;
   
-  
-//console.assert(assertEqual("Lighthouse Labs", "Boot camp"))
-// TEST CODE
-//   assertEqual("Lighthouse Labs", "Bootcamp");
-//   assertEqual(1, 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
