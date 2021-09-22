359021bdefe6110baffa317d68a5a9c09d57ff3a
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-21 19:16:56 -0600

    unit testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,21 @@
 const middle = require('../middle')
 const assertArraysEqual = require ('../assertArraysEqual')
+const { assert } = require('chai')
 
 
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]) 
-assertArraysEqual(middle([1, 2]),[]) 
+
+describe("middle",()=>{
+    it("return 2,3 for [1,2,3,4,5]",()=>{
+assert.deepEqual(middle([1,2,3,4,]),[2,3])
+    });
+    it("returns [3] for [1,2,3,4,5]",()=>{
+assert.deepEqual(middle([1,2,3,4,5])[3])
+    });
+    it("returns [] for [1,2]",()=>{
+        assert.deepEqual(middle([1,2]),[])
+    })
+})
+
+// assertArraysEqual(middle([1, 2, 3, 4]),[2,3])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]) 
+// assertArraysEqual(middle([1, 2]),[]) 
