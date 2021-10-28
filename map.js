ddd15449f9182f3ed5a2fa329f220cf8c8719415
Luis Andres Contreras <tremenditomen@gmail.com>
2021-10-28 17:37:45 -0600

    adding map.js

--- map.js
+++ map.js
@@ -1,22 +1,12 @@
-const map = function(array, callback) {
-
-    const results = [];
-    for (let item of array) {
-      results.push(callback(item));
-    }
-    return results;
-  
-  };
-  
-
-
-
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
 
-
-
-const results1 = map(words, word => word[0]);
+const results1 = map(words, (word) => word[0]);
 console.log(results1);
-
-
